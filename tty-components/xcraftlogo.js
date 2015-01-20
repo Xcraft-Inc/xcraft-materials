ef4cbf1552891a077b7c0645328360e5d213e6fd
Mathieu Schroeter <mathieu@schroetersa.ch>
2015-01-20 19:25:52 +0100

    Fixes alignment with the awesome Xcraft logo.

--- tty-components/xcraftlogo.js
+++ tty-components/xcraftlogo.js
@@ -1,12 +1,17 @@
+'use strict';
+
 var actions = require ('../actions/ttyComponentsActions.js');
 var displayLogo = actions.displayLogo;
+
 module.exports = function () {
-  displayLogo.listen(function () {
-    console.log ('\n____  ___                   _____  __   ');
-    console.log ('\\   \\/  /________________ _/ ____\\/  |_ ');
-    console.log (' \\     // ___\\_  __ \\__  \\\\   __\\\\   __\\');
-    console.log (' /     \\  \\___|  | \\// __ \\|  |   |  |  ');
-    console.log ('/___/\\  \\___  >__|  (____  /__|   |__|  ');
-    console.log ('    \\_/   \\/           \\/            \n');
+  displayLogo.listen (function () {
+    console.log ();
+    console.log (' ____  ___                   _____  __');
+    console.log (' \\   \\/  /________________ _/ ____\\/  |_');
+    console.log ('  \\     // ___\\_  __ \\__  \\\\   __\\\\   __\\');
+    console.log ('  /     \\  \\___|  | \\// __ \\|  |   |  |');
+    console.log (' /___/\\  \\___  >__|  (____  /__|   |__|');
+    console.log ('       \\_/   \\/           \\/');
+    console.log ();
   });
 };
