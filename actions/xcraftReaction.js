d4312da3f55b980728684b0efc21146d7a283989
Mathieu Schroeter <schroeter@epsitec.ch>
2015-01-29 13:22:40 +0100

    Split xcraftReaction in two functions for readability.

--- actions/xcraftReaction.js
+++ actions/xcraftReaction.js
@@ -4,40 +4,51 @@ var Reflux = require ('reflux');
 var xLog   = require ('xcraft-core-log')('xcraft-materials');
 var xUtils = require ('xcraft-core-utils');
 
+
+var listenerAxon = function (commands, events, busClient) {
+  xLog.verb ('Xcraft reaction listening using Xcraft-busclient...');
+
+  commands.send.listen (function (cmdData) {
+    xLog.verb  (cmdData.cmd + ' reaction send to bus: ');
+    busClient.command.send (cmdData.cmd);
+  });
+
+  busClient.subscriptions.on ('message', function (topic, msg) {
+    var action;
+    if (msg) {
+      action = xUtils.topic2Action (topic);
+      if (events[action]) {
+        events[action] (msg.data);
+      }
+    }
+  });
+};
+
+var listenerIpc = function (commands, events) {
+  var ipc = require ('ipc');
+
+  console.log ('Xcraft reaction listening using IPC...');
+
+  ipc.send ('subscribe-event', 'pacman.list');
+  ipc.send ('subscribe-event', 'activity.started');
+
+  commands.send.listen (function (cmdData) {
+    console.log (cmdData.cmd + ' reaction send to IPC');
+    ipc.send ('send-cmd', cmdData.cmd);
+  });
+
+  ipc.on('trigger-event', function (event) {
+    events[event.name] (event.msg.data);
+  });
+};
+
 module.exports = function (busClient) {
   var commands = require ('./xcraftCommands.js');
   var events   = require ('./xcraftEvents.js');
 
   if (busClient) {
-    xLog.verb ('Xcraft reaction listening using Xcraft-busclient...');
-    commands.send.listen (function (cmdData) {
-      xLog.verb  (cmdData.cmd + ' reaction send to bus: ');
-      busClient.command.send (cmdData.cmd);
-    });
-
-    busClient.subscriptions.on ('message', function (topic, msg) {
-      var action;
-      if (msg) {
-        action = xUtils.topic2Action (topic);
-        if (events[action]) {
-          events[action] (msg.data);
-        }
-      }
-    });
+    listenerAxon (commands, events, busClient);
   } else {
-    var ipc = require ('ipc');
-    console.log ('Xcraft reaction listening using IPC...');
-
-    ipc.send ('subscribe-event', 'pacman.list');
-    ipc.send ('subscribe-event', 'activity.started');
-
-    commands.send.listen (function (cmdData) {
-      console.log (cmdData.cmd + ' reaction send to IPC');
-      ipc.send ('send-cmd', cmdData.cmd);
-    });
-
-    ipc.on('trigger-event', function (event) {
-      events[event.name] (event.msg.data);
-    });
+    listenerIpc (commands, events);
   }
 };
