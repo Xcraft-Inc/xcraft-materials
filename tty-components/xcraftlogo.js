d32dd35f41a99b2db0142dbc4d9d369a4e137d2d
Mathieu Schroeter <mathieu@schroetersa.ch>
2015-01-31 14:16:02 +0100

    Improve the Xcraft tty logo.
    Write dynamically add colors.

--- tty-components/xcraftlogo.js
+++ tty-components/xcraftlogo.js
@@ -4,14 +4,33 @@ var actions = require ('../actions/ttyComponentsActions.js');
 var displayLogo = actions.displayLogo;
 
 module.exports = function () {
+  var clc    = require ('cli-color');
+  var figlet = require ('figlet');
+
   displayLogo.listen (function () {
-    console.log ();
-    console.log (' ____  ___                   _____  __');
-    console.log (' \\   \\/  /________________ _/ ____\\/  |_');
-    console.log ('  \\     // ___\\_  __ \\__  \\\\   __\\\\   __\\');
-    console.log ('  /     \\  \\___|  | \\// __ \\|  |   |  |');
-    console.log (' /___/\\  \\___  >__|  (____  /__|   |__|');
-    console.log ('       \\_/   \\/           \\/');
-    console.log ();
+    figlet ('Xcraft', {
+      font: 'graffiti',
+      horizontalLayout: 'default',
+      verticalLayout: 'default'
+    }, function (err, data) {
+      console.log ('');
+      console.log (data .replace (/[_\/\\]/g, function (match) {
+        switch (match) {
+        case '_': {
+          return clc.green (match);
+        }
+        case '/': {
+          return clc.greenBright (match);
+        }
+        case '\\': {
+          return clc.blackBright (match);
+        }
+        case '|': {
+          return clc.white (match);
+        }
+        }
+      }));
+      console.log ('');
+    });
   });
 };
