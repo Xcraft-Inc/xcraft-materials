9a5b18028eb8f07a6a30af5de3e225a4034ad12a
Mathieu Schroeter <mathieu@schroetersa.ch>
2015-01-31 16:58:43 +0100

    Cosmetic: for consistency.

--- tty-components/packagelist.js
+++ tty-components/packagelist.js
@@ -5,12 +5,12 @@ var util           = require ('util');
 
 module.exports = function () {
   packagesStore.listen (function (list) {
-    var header = util.format (' name%s version%s architectures',
+    var header = util.format ('name%s version%s architectures',
                               new Array (40 - 'name'.length).join (' '),
                               new Array (15 - 'version'.length).join (' '));
     console.log ();
     console.log (header);
-    console.log (' ' + new Array (header.length + 1).join ('-'));
+    console.log (new Array (header.length + 1).join ('-'));
 
     list.forEach (function (def) {
       var version = def.version.toString ();
@@ -18,7 +18,7 @@ module.exports = function () {
         version = version.substr (0, 11) + '...';
       }
 
-      console.log (' %s%s %s%s',
+      console.log ('%s%s %s%s',
                    def.name,
                    new Array (40 - def.name.length).join (' '),
                    version,
