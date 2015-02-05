6e251f74bbba06ab8f685c709d9e7c1287022bc4
Samuel Loup <loup@epsitec.ch>
2015-02-05 20:12:15 +0100

    Cosmetics

--- tty-components/activitylist.js
+++ tty-components/activitylist.js
@@ -1,6 +1,6 @@
 'use strict';
-var Reflux     = require ('reflux');
-var activityStore = Reflux.createStore(require ('../stores/activitystore.js')());
+var reflux        = require ('reflux');
+var activityStore = reflux.createStore(require ('../stores/activitystore.js')());
 
 
 module.exports = function () {
