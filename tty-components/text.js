e3018ae4d33a14306829c54156571ec7f0257eb0
Mathieu Schroeter <schroeter@epsitec.ch>
2015-06-29 11:33:45 +0200

    Improve the output of widget.text.*.

--- tty-components/text.js
+++ tty-components/text.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var util   = require ('util');
 var reflux = require ('reflux');
 var clc    = require ('cli-color');
 
@@ -14,6 +15,24 @@ module.exports = function () {
   };
 
   textStore.listen (function (mode, data) {
-    console.log ('%s%s', clc.redBright (data.prefix) || '', colors[mode] (data.text));
+    var len = data.prefix.length + data.mod.length + 4;
+    var max = 35;
+    len = max - len;
+    if (len < 0) {
+      len = 0;
+    }
+
+    var spaces = mode.length < 4 ? '  ' : ' ';
+    var begin = util.format ('%s [%s%s] %s:%s',
+                             clc.blackBright (data.prefix),
+                             clc.whiteBright.bold (data.mod),
+                             new Array (len).join (clc.blackBright ('.')),
+                             colors[mode] (mode.charAt (0).toUpperCase () + mode.slice (1)),
+                             spaces);
+    var offset = data.prefix.length + data.mod.length + len + mode.length + spaces.length + 5;
+    var indent = new Array (offset).join (' ');
+    var text = data.text.replace (/\n/g, '\n' + indent);
+
+    console.log ('%s%s', begin, text);
   });
 };
