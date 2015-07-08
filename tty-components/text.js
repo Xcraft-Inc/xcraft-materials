51f704108cd5cffaf232a03f20e9fef9027f09a1
Mathieu Schroeter <schroeter@epsitec.ch>
2015-07-08 12:09:25 +0200

    Indent properly the log (like a wordwrap).
    It cuts between [ ] and [/].

--- tty-components/text.js
+++ tty-components/text.js
@@ -3,12 +3,13 @@
 var util   = require ('util');
 var reflux = require ('reflux');
 var clc    = require ('cli-color');
+var ansiRegex = require ('ansi-regex');
+
+var xUtils = require ('xcraft-core-utils');
 
 var textStore = reflux.createStore (require ('../stores/textstore.js'));
 
 module.exports = function () {
-  var xUtils = require ('xcraft-core-utils');
-
   var colors = {
     verb: clc.cyanBright.bold,
     info: clc.greenBright.bold,
@@ -33,8 +34,24 @@ module.exports = function () {
                              spaces);
     var offset = data.prefix.length + data.mod.length + len + mode.length + spaces.length + 5;
     var indent = new Array (offset).join (' ');
-    var text = data.text.replace (/\n/g, '\n' + indent);
+    var text = data.text.replace (/\n/g, ' \n' + indent) + ' ';
 
-    console.log ('%s%s', begin, text);
+    var beginLength = begin.replace (ansiRegex (), '').length;
+    var availableSpace = clc.windowSize.width - beginLength - 1;
+    if (availableSpace <= 0) {
+      console.log ('%s%s', begin, text);
+      return;
+    }
+
+    spaces = new Array (beginLength + 1).join (' ');
+    /* Example with an available space of 120 chars.
+     * (.{1,119}[ /]|.{120})
+     */
+    var regex = new RegExp ('(.{1,' + (parseInt (availableSpace) - 1) + '}[ /]|.{' + availableSpace + '})', 'g');
+    var matches = text.match (regex) || [text];
+    matches.forEach (function (part) {
+      console.log ('%s%s', begin, part);
+      begin = spaces;
+    });
   });
 };
