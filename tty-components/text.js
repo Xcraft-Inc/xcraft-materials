17ed67c2869e41276993756c0061d17fec40ba0a
Mathieu Schroeter <schroeter@epsitec.ch>
2015-07-24 14:33:13 +0200

    Move the output log code in a more generic function.
    Then this function will be useful for the GitLab CI runner too.

--- tty-components/text.js
+++ tty-components/text.js
@@ -1,56 +1,16 @@
 'use strict';
 
-var util   = require ('util');
 var reflux = require ('reflux');
 var clc    = require ('cli-color');
-var ansiRegex = require ('ansi-regex');
-
-var xUtils = require ('xcraft-core-utils');
 
 var textStore = reflux.createStore (require ('../stores/textstore.js'));
-var indent = 16;
 
 module.exports = function () {
-  var colors = {
-    verb: clc.cyanBright.bold,
-    info: clc.greenBright.bold,
-    warn: clc.yellowBright.bold,
-    err:  clc.redBright.bold
-  };
+  var log = require ('xcraft-core-utils').log;
 
   textStore.listen (function (mode, data) {
-    var len = data.prefix.length + data.mod.length + 2;
-    len = indent - len;
-    if (len < 0) {
-      indent += -len + 1;
-      len = 0;
-    }
-
-    var spaces = mode.length < 4 ? '  ' : ' ';
-    var begin = util.format ('%s [%s%s] %s:%s',
-                             data.prefix,
-                             clc.whiteBright.bold (data.mod),
-                             new Array (len).join (clc.blackBright ('.')),
-                             colors[mode] (xUtils.string.capitalize (mode)),
-                             spaces);
-    var text = data.text.replace (/\n/g, ' \n') + ' ';
-
-    var beginLength = begin.replace (ansiRegex (), '').length;
-    var availableSpace = clc.windowSize.width - beginLength - 1;
-    if (availableSpace <= 0) {
-      console.log ('%s%s', begin, text);
-      return;
-    }
-
-    spaces = new Array (beginLength + 1).join (' ');
-    /* Example with an available space of 120 chars.
-     * (.{1,119}[ /\\\\]|.{120})
-     */
-    var regex = new RegExp ('(.{1,' + (parseInt (availableSpace) - 1) + '}[ /\\\\]|.{' + availableSpace + '})', 'g');
-    var matches = text.match (regex) || [text];
-    matches.forEach (function (part) {
-      console.log ('%s%s', begin, part);
-      begin = spaces;
-    });
+    process.stdout.write (clc.erase.lineLeft);
+    process.stdout.write (clc.move.left (clc.windowSize.width - 1));
+    console.log (log.decorate (mode, data.prefix, data.mod, data.text));
   });
 };
