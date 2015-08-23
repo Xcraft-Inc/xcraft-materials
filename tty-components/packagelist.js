a019f11bd71683d82f6143013e63042540c9fca9
Mathieu Schroeter <mathieu@schroetersa.ch>
2015-08-23 16:06:42 +0200

    Use ascii-table for the list of package definitions.

--- tty-components/packagelist.js
+++ tty-components/packagelist.js
@@ -3,30 +3,20 @@
 var reflux         = require ('reflux');
 var packagesStore  = reflux.createStore (require ('../stores/packagesstore.js'));
 
-var util           = require ('util');
+var AsciiTable = require ('ascii-table');
+var log        = require ('xcraft-core-utils').log;
 
 module.exports = function () {
   packagesStore.listen (function (list) {
-    var header = util.format ('name%s version%s architectures',
-                              new Array (40 - 'name'.length).join (' '),
-                              new Array (15 - 'version'.length).join (' '));
-    console.log ();
-    console.log (header);
-    console.log (new Array (header.length + 1).join ('-'));
+    var table = new AsciiTable ('List of package definitions');
+
+    table.setHeading ('Name', 'Version', 'Architectures');
 
     list.forEach (function (def) {
       var version = def.version.toString ();
-      if (def.version.toString ().length > 14) {
-        version = version.substr (0, 11) + '...';
-      }
-
-      console.log ('%s%s %s%s',
-                   def.name,
-                   new Array (40 - def.name.length).join (' '),
-                   version,
-                   new Array (15 - version.length).join (' '),
-                   def.architecture.join (', '));
+      table.addRow (def.name, version, def.architecture.join (', '));
     });
-    console.log ();
+
+    console.log (table.toString ());
   });
 };
