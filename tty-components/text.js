f26d0bff16de7cd1e82caf880d944b4f4f9c5283
Mathieu Schroeter <mathieu@schroetersa.ch>
2016-12-26 17:44:44 +0100

    Do not repeat the empty line with embedded logs when it's useless
    When it begins with the same text, we keep only the line for the first
    time.

--- tty-components/text.js
+++ tty-components/text.js
@@ -8,9 +8,19 @@ var textStore = reflux.createStore (require ('../stores/textstore.js'));
 module.exports = function () {
   var log = require ('xcraft-core-utils').log;
 
+  let previous = null;
+
   textStore.listen (function (mode, data) {
+    const begin = mode + data.prefix + data.mod;
+    const stripBegin = begin === previous;
+
+    /* Do not repeat the begining for each line in the case of embedded logs */
+    if (!stripBegin) {
+      previous = begin;
+    }
+
     process.stdout.write (clc.erase.lineLeft);
     process.stdout.write (clc.move.left (clc.windowSize.width - 1));
-    console.log (log.decorate (mode, data.prefix, data.mod, data.text));
+    console.log (log.decorate (mode, data.prefix, data.mod, data.text, null, stripBegin));
   });
 };
