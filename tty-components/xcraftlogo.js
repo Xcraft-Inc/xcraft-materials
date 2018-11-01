df66438940e1b31acb70c19accb9ab56ae4feb83
Mathieu Schroeter <mathieu@schroetersa.ch>
2018-11-01 07:50:10 +0100

    Use graffiti from xUtils

--- tty-components/xcraftlogo.js
+++ tty-components/xcraftlogo.js
@@ -1,47 +1,10 @@
 'use strict';
 
+const {log} = require('xcraft-core-utils');
+
 var actions = require('../actions/ttyComponentsActions.js');
 var displayLogo = actions.displayLogo;
 
 module.exports = function() {
-  var clc = require('cli-color');
-  var figlet = require('figlet');
-
-  displayLogo.listen(function() {
-    figlet(
-      'Xcraft',
-      {
-        font: 'Graffiti',
-        horizontalLayout: 'default',
-        verticalLayout: 'default',
-      },
-      function(err, data) {
-        if (err) {
-          console.error(err);
-          return;
-        }
-
-        console.log();
-        console.log(
-          data.replace(/[_\/\\]/g, function(match) {
-            switch (match) {
-              case '_': {
-                return clc.green(match);
-              }
-              case '/': {
-                return clc.greenBright(match);
-              }
-              case '\\': {
-                return clc.blackBright(match);
-              }
-              case '|': {
-                return clc.white(match);
-              }
-            }
-          })
-        );
-        console.log();
-      }
-    );
-  });
+  displayLogo.listen(() => log.graffiti('Xcraft'));
 };
